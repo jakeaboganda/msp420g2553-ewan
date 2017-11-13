8559747c8d5843c738d8f784b1b3a4410aa3e127
jaketenten <jake@mytenten.com>
2017-11-13 11:53:35 +0900

    synchronize local environment

--- Debug/subdir_rules.mk
+++ Debug/subdir_rules.mk
@@ -3,13 +3,6 @@
 ################################################################################
 
 # Each subdirectory must supply rules for building sources it contributes
-circular_buffer.obj: ../circular_buffer.c $(GEN_OPTS) | $(GEN_HDRS)
-	@echo 'Building file: $<'
-	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccsv6/ccs_base/msp430/include" --include_path="C:/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include" --advice:power=all -g --define=__MSP430G2553__ --diag_warning=225 --diag_wrap=off --display_error_number --printf_support=minimal --preproc_with_compile --preproc_dependency="circular_buffer.d" $(GEN_OPTS__FLAG) "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: $<'
 	@echo 'Invoking: MSP430 Compiler'
--- Release/subdir_vars.mk
+++ Release/subdir_vars.mk
@@ -7,21 +7,27 @@ CMD_SRCS += \
 ../lnk_msp430g2553.cmd 
 
 C_SRCS += \
+../circular_buffer.c \
 ../main.c 
 
 OBJS += \
+./circular_buffer.obj \
 ./main.obj 
 
 C_DEPS += \
+./circular_buffer.d \
 ./main.d 
 
 C_DEPS__QUOTED += \
+"circular_buffer.d" \
 "main.d" 
 
 OBJS__QUOTED += \
+"circular_buffer.obj" \
 "main.obj" 
 
 C_SRCS__QUOTED += \
+"../circular_buffer.c" \
 "../main.c" 
 
 
